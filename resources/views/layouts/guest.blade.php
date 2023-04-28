911134cb3f9e9b2633584f164722503169901960
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2023-04-28 13:53:43 +0530

    Update Guest Blade File
    : Condition to display the login and register route in the guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -25,6 +25,18 @@
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
+
+            <div class="mt-2">
+                @if(Route::currentRouteName() == 'login')
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+                    {{ __('Not have an account?') }}
+                </a>
+                @elseif(Route::currentRouteName() == 'register')
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+                @endif
+            </div>
         </div>
     </body>
 </html>
