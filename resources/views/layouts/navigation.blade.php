9a6263f54e71466a79a2d7bab163d3da1622a3a9
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2023-04-28 13:54:18 +0530

    Update Navigation Blade File
    : Included default avatar image for users

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,8 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        {{-- <x-application-logo class="block fill-current text-gray-800 dark:text-gray-200" /> --}}
+                        <img src={{ asset('img/logo.png') }} class="" alt="Y@ACT" heigh="50px" width="50px">
                     </a>
                 </div>
 
@@ -23,7 +24,8 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
+                            <img src="{{ asset('img/icons/user.png') }}" class="w-9 h-9 rounded" alt="{{ auth()->user()->name }}">
+                            <div class="ml-2">{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
