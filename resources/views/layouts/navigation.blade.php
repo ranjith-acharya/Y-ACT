8a3f8bec4dbf6bdf68cf4765af35a2d00888c462
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2023-04-30 14:55:43 +0530

    Changes made for Routing based on Role

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,7 +24,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <img src="{{ asset('img/icons/user.png') }}" class="w-9 h-9 rounded" alt="{{ auth()->user()->name }}">
+                            <img src="../img/profile-picture/{{ Auth::user()->avatar }}" class="w-9 h-9 rounded" alt="{{ auth()->user()->name }}">
                             <div class="ml-2">{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
@@ -69,7 +69,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="(auth()->user()->role === 'admin') ? route('admin.dashboard') : route('dashboard')" :active="(auth()->user()->role === 'admin') ? request()->routeIs('admin.dashboard') : route('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
@@ -82,7 +82,7 @@
             </div>
 
             <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
+                <x-responsive-nav-link :href="(auth()->user()->role === 'admin') ? route('admin.profile.edit') : route('profile.edit')">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
 
