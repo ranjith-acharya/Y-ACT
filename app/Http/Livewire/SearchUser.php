997d08dbefbca35985b9efddb7ac6989a2fe48b7
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2023-05-01 22:38:08 +0530

    Update Livewire Search Component
    : Route conditioning if ADMIN than sort based on name
    : if MEMBER only members sort based on name

--- app/Http/Livewire/SearchUser.php
+++ app/Http/Livewire/SearchUser.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class SearchUser extends Component
 {
@@ -13,9 +14,16 @@ class SearchUser extends Component
 
         $users = [];
         if(strlen($this->search) > 0){
-            $users = User::where('name', 'LIKE', '%'. $this->search. '%')
+            if(Auth::user()->role === 'admin'){
+                $users = User::where('name', 'LIKE', '%'. $this->search. '%')
+                        ->orderBy('name', 'asc')
+                        ->get();
+            }else{
+                $users = User::where('name', 'LIKE', '%'. $this->search. '%')
                     ->where('role', 'member')
+                    ->orderBy('name', 'asc')
                     ->get();
+            }
         }        
         return view('livewire.search-user', [
             'users' => $users
