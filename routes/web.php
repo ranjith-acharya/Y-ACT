68de41ed0bb01b244777b914c4a8afdb951074da
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2023-05-01 22:42:06 +0530

    Update Application Routing
    : Included resource view controller for both Admin and Member

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ViewController;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,7 @@ Route::middleware(['auth', 'verified'])->group(function(){
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('view', ViewController::class);
 });
 
 Route::middleware(['auth'])->group(function () {
@@ -35,6 +37,7 @@ Route::middleware(['auth'])->group(function () {
             Route::get('/profile', [App\Http\Controllers\Admin\ProfileController::class, 'edit'])->name('profile.edit');
             Route::patch('/profile', [App\Http\Controllers\Admin\ProfileController::class, 'update'])->name('profile.update');
             Route::delete('/profile', [App\Http\Controllers\Admin\ProfileController::class, 'destroy'])->name('profile.destroy');
+            Route::resource('member/view', ViewController::class);
         });
     });
 });
