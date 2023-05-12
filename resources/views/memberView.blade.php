e67e9c2fbeb5ab815e201173c4f8d17ebd4ce9f3
RanjithKrishna Acharya <ranjithacharya997@gmail.com>
2023-05-12 16:20:32 +0530

    Update MemberView Blade File

--- resources/views/memberView.blade.php
+++ resources/views/memberView.blade.php
@@ -5,13 +5,15 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ $user->name }}
+            <img src="../../../img/profile-picture/{{ $user->avatar }}" class="w-10 h-10 rounded-full inline-block" alt="{{ $user->name }}"><span class="ml-2">{{ $user->name }}</span>
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            
+            <div class="dark:bg-gray-700 text-white rounded p-3">
+                {{ $user->name }}<br>{{ $user->email }}
+            </div>
         </div>
     </div>
 </x-app-layout>
